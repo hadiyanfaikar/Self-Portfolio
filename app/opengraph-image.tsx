8c18c8c90bf2806128e639bfa73ec629fcdc4718
Hadiyan Faikar <faikar1998@gmail.com>
2026-06-13 14:37:55 +0700

    update opengraph

--- app/opengraph-image.tsx
+++ app/opengraph-image.tsx
@@ -1,8 +1,6 @@
 import { ImageResponse } from "next/og";
 import { siteConfig } from "@/lib/constants";
 
-export const runtime = "edge";
-
 export const alt = `${siteConfig.name} — ${siteConfig.title}`;
 export const size = { width: 1200, height: 630 };
 export const contentType = "image/png";
